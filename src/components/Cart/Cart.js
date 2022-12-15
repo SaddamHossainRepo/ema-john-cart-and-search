6561f6c1dd940b2f7a943e98cb96208fe1e2c3b5
Saddam <saddam13@cse.pstu.ac.bd>
2022-12-15 12:49:45 +0600

    calculate shopping cart price and grand total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    const { cart } = props;
+
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = total * 0.1;
+    const grandTotal = total+shipping+tax;
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary in Cart</h4>
-            <p>Selected Items: {props.cart.length}</p>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
         </div>
     );
 };
